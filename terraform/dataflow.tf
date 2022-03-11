461f42fa16334cf3b82f19cf5dfdb65fd79f0cc7
Pavlo Bagriy <bagriy.pavlo@gmail.com>
2022-03-11 12:14:56 +0200

    Add cloudbuild resources

--- terraform/dataflow.tf
+++ terraform/dataflow.tf
@@ -7,4 +7,14 @@ resource "google_dataflow_flex_template_job" "pubsub_to_bigquery" {
     outputTable       = "${var.PROJECT_NAME}:${google_bigquery_dataset.learning_dataset.dataset_id}.data"
   }
   on_delete = "cancel"
+
+  depends_on = [null_resource.build_template]
+}
+
+resource "null_resource" "build_template" {
+
+  provisioner "local-exec" {
+    command = "sbt createFlexTemplate"
+    working_dir = "../dataflow-job"
+  }
 }
