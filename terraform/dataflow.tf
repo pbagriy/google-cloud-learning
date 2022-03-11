2d330405994a18ace0941b3707dbd2ad19965beb
Pavlo Bagriy <bagriy.pavlo@gmail.com>
2022-03-11 13:29:26 +0200

    Fix name of dataflow template in terraform

--- terraform/dataflow.tf
+++ terraform/dataflow.tf
@@ -1,7 +1,7 @@
 resource "google_dataflow_flex_template_job" "pubsub_to_bigquery" {
   provider                = google-beta
   name                    = "pubsub-to-bigquery"
-  container_spec_gcs_path = "gs://${var.CONFIG_BUCKET}/dataflow/templates/PubSubToBigQuery.json"
+  container_spec_gcs_path = "gs://${var.CONFIG_BUCKET}/dataflow/templates/dataflow-job.json"
   parameters              = {
     inputSubscription = google_pubsub_subscription.data-topic-subscription-dataflow.id
     outputTable       = "${var.PROJECT_NAME}:${google_bigquery_dataset.learning_dataset.dataset_id}.data"
